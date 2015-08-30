933b90f7da0a4aa5221b3de2cd788afa9edc6d0e
PietjePrecies <menuma@gmail.com>
2015-08-30 19:28:05 +0200

    SimpleScrape Wikipedia Kevin Bacon

---
+++ WebScrapingSimple.py
@@ -0,0 +1,9 @@
+from urllib import urlopen
+from bs4	import	BeautifulSoup
+
+html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
+
+bsObj = BeautifulSoup(html)
+for link in bsObj.findAll("a"):
+	if 'href' in link.attrs:
+		print(link.attrs['href'])
